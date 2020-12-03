68e55d3f42f4bd32ecbcb5d2faa16f6a76231ea1
Prachi Suryawanshi <prachis@chegg.com>
2020-12-04 01:22:44 +0530

    Initial Commit

---
+++ Controllers/TodoItemsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TodoItemsController : ControllerBase
+  {
+    private readonly TodoContext _context;
+
+    public TodoItemsController(TodoContext context)
+    {
+      _context = context;
+    }
+
+    // GET: api/TodoItems/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+    {
+      var todoItem = await _context.TodoItems.FindAsync(id);
+
+      if (todoItem == null)
+      {
+        return NotFound();
+      }
+
+      return todoItem;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<TodoItem>>> GetAll()
+    {
+      return await _context.TodoItems.ToListAsync();
+    }
+  }
+}
---
+++ Migrations/20201203192223_initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TodoApi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    IsComplete = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+        }
+    }
+}
